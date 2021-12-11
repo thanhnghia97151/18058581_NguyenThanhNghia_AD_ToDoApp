a824a50f1f2e8adac1aa2c3a163ad8c7e4f0efff
thanhnghia97151 <nguyenthanhnghia0907@gmail.com>
2021-12-11 13:01:49 +0700

    update

--- app/src/main/java/com/example/a18058581_nguyenthanhnghia_ad_todoapp/FirebaseUtils.kt
+++
@@ -1,9 +0,0 @@
-package com.example.a18058581_nguyenthanhnghia_ad_todoapp
-
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-
-object FirebaseUtils {
-    val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
-    val firebaseUser: FirebaseUser? = firebaseAuth.currentUser
-}
